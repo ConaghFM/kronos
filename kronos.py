499d80889df27b6b9230e903a3e17719fd384f9a
Conagh Fitzgerald-Mansell <conagh.fitzgerald@perfectpattern.de>
2020-11-24 01:06:49 +1300

    rewrote timeFunction with difference eqn

--- kronos.py
+++ kronos.py
@@ -1,42 +1,45 @@
-from gpiozero import *
+from gpiozero import * # https://gpiozero.readthedocs.io/en/stable/recipes.html
 from time import *
 from numpy import *
 
-positive = LED(17) # Board 11
-negative = LED(27) # Board 13
+POSITIVE = LED(17) # Board 11
+NEGATIVE = LED(27) # Board 13
+SLEEP_TIME = 0.01 # 10 ms
+clockStart = time.time() # seconds since the clock started, as a float
+lastTime = 0 # the last time timeFunction executed, as a float
 cycleLength = 1 # Tick length in seconds
 
 while(true)
-    positive.on()
-    sleep(0.01) # 10 ms
-    positive.off()
-    negative.on()
-    sleep(0.01)
-    negative.off()
-    if(cycleLength-0.02 > 0) # check this time is not negative
-        sleep(cycleLength-0.02)
+    POSITIVE.on()
+    sleep(SLEEP_TIME)
+    POSITIVE.off()
+    NEGATIVE.on()
+    sleep(SLEEP_TIME)
+    NEGATIVE.off()
+    if(cycleLength-(2*SLEEP_TIME) > 0): # check this time is not NEGATIVE
+        sleep(cycleLength-(2*SLEEP_TIME))
     cycleLength = timeFunction()
 
-# keep this format, and just change squared() for your math function
-# this keeps the rate at 1 if the function just returns the current number of seconds
+# keep this format 1/(func(seconds) - func(seconds-cycleLength)), and just change out the math function
+# returns the fraction of a second each tick should last
 def timeFunction(cycleLength):
-    seconds = getSeconds
-    return seconds/squared(seconds)
+    seconds = getSeconds()
+    frequency = squared(seconds) - squared(lastTime) # the tick frequency in Hz
+    lastTime = seconds
+    return 1/frequency
 
-# this will reach our 20ms limit after about 50 seconds
+# this will reach our 20ms limit very quickly
 def squared(seconds):
     return seconds**2
 
 def logged(seconds):
     return numpy.log(seconds)
 
-# Seconds since 00:00 on the clock
+# Seconds since 00:00 on the clock as a float
 def getSeconds():
-    t = time.localtime
-    if(t.tm_hours > 11)
-        hours = t.tm_hour - 12
-    else
-        hours = t.tm_hour
-    t0 = time.struct_time(t.tm_year, t.tm_mon, t.tm_day, hours, 0, 0, tm_wday=4, tm_yday=60, tm_isdst=0)
-    seconds = time.mktime(t-t0)
-    return seconds
+    if ((time.time() - clockStart) > 43200): # reset every 12 hours
+        clockStart = time.time()
+        lastTime = 0
+        return 0
+    else:
+        return time.time() - clockStart
