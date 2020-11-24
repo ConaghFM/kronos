ac91f6cdb15b273653a3c3d7aadd0f11272f7573
Conagh Fitzgerald-Mansell <conagh.fitzgerald@perfectpattern.de>
2020-11-24 16:59:18 +1300

    syntax fixing

--- kronos.py
+++ kronos.py
@@ -8,22 +8,22 @@ SLEEP_TIME = 0.01 # 10 ms
 clockStart = time.time() # seconds since the clock started, as a float
 cycleLength = 1 # Tick length in seconds
 
-while(true)
+while True:
     POSITIVE.on()
     sleep(SLEEP_TIME)
     POSITIVE.off()
     NEGATIVE.on()
     sleep(SLEEP_TIME)
     NEGATIVE.off()
-    if(cycleLength-(2*SLEEP_TIME) > 0): # check this time is not NEGATIVE
-        sleep(cycleLength-(2*SLEEP_TIME))
+    if cycleLength - (2*SLEEP_TIME) > 0 : # check this time is not NEGATIVE
+        sleep(cycleLength - (2*SLEEP_TIME))
     cycleLength = timeFunction()
 
 # keep this format 1/(func(seconds) - func(seconds-cycleLength)), and just change out the math function
 # returns the fraction of a second each tick should last
 def timeFunction():
     seconds = getSeconds()
-    if (seconds < 1): # avoid feeding negative times to the math functions
+    if seconds < 1 : # avoid feeding negative times to the math functions
         frequency = 1
     else:
         frequency = squared(seconds) - squared(seconds-1) # the tick frequency in Hz
@@ -38,7 +38,7 @@ def logged(seconds):
 
 # Seconds since 00:00 on the clock as a float
 def getSeconds():
-    if ((time.time() - clockStart) > 43200): # reset every 12 hours
+    if time.time() - clockStart > 43200 : # reset every 12 hours
         clockStart = time.time()
         return 0
     else:
