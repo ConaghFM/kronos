e615cb7a9ca2f1d56a3ed0357c84d67bf8753b22
Conagh Fitzgerald-Mansell <conagh.fitzgerald@perfectpattern.de>
2020-11-27 14:34:54 +1300

    tuning SLEEP_TIME

--- kronos.py
+++ kronos.py
@@ -4,7 +4,7 @@ from numpy import * # https://numpy.org/doc/
 
 NEGATIVE = LED(17) # Board 11
 POSITIVE = LED(27) # Board 13
-SLEEP_TIME = 0.005 # 10 ms
+SLEEP_TIME = 0.013 # 10 ms
 clockStart = time() # seconds since the clock started, as a float
 cycleLength = 1 # Tick length in seconds
 
